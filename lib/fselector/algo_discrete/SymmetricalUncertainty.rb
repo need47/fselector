d22912aa8267d065a2d51486e7c2367f2f3f3077
Tiejun Cheng <need47@gmail.com>
2012-03-29 10:38:24 -0400

    add SymmetricalUncertainty algo

--- lib/fselector/algo_discrete/SymmetricalUncertainty.rb
+++ lib/fselector/algo_discrete/SymmetricalUncertainty.rb
@@ -22,85 +22,8 @@ module FSelector
   
     # calculate contribution of each feature (f) across all classes
     def calc_contribution(f)
-      # step 1: H(c)
-      hc = 0.0
-      n = get_sample_size.to_f
+      hc, hcf, hf = get_Hc, get_Hcf(f), get_Hf(f)
       
-      each_class do |k|
-        nk = get_data[k].size
-        p1 = nk/n
-
-        if p1.zero?
-          hc += -0.0
-        else
-          hc += -1.0 * (p1 * Math.log2(p1))
-        end
-      end
-      
-      # step 2: H(c|f)
-      hcf = 0.0
-      m = {}
-      
-      each_class do |k|
-        nk = get_data[k].size        
-        nv = 0.0
-        
-        fvs = get_feature_values(f).uniq
-        fvs.each do |v|
-          a, b = get_Av(f, k, v), get_Bv(f, k, v)
-          #pp "(v,a,b) => (#{v}, #{a}, #{b})"
-          nv += a
-          
-          p2 = a/(a+b)
-          p3 = (a+b)/n
-          
-          if p2.zero?
-            hcf += -0.0
-          else
-            hcf += -1.0 * p3 * (p2 * Math.log2(p2))
-          end           
-        end
-        
-        m[k] = nk - nv
-         
-      end
-      
-      # handle empty feature for each class
-      sm = m.values.sum
-      if not sm.zero?
-        #pp m
-        m.each do |k, i|
-          pm = i/sm
-          
-          if pm.zero?
-            hcf += -0.0
-          else
-            hcf += -1.0 * (sm/n) * (pm * Math.log2(pm))
-          end
-        end
-      end
-      
-      # step 3: H(f)
-      hf = 0.0
-      fvs = get_feature_values(f)
-      fvs.uniq.each do |v|
-        p4 = fvs.count(v)/n
-        if p4.zero?
-          hf += -0.0
-        else
-          hf += -1.0 * (p4 * Math.log2(p4))
-        end
-      end
-      
-      # handle empty feature
-      p5 = (n-fvs.size)/n
-      if p5.zero?
-        hf += -0.0
-      else
-        hf += -1.0 * (p5 * Math.log2(p5))
-      end
-      
-      # step 4: score
       s =  2*(hc-hcf)/(hc+hf)
      
       set_feature_score(f, :BEST, s)      
