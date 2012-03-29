3049606e818e6a2db4b2978d9f267b89e814bb06
Tiejun Cheng <need47@gmail.com>
2012-03-29 19:32:54 -0400

    add base class BaseReliefF

--- lib/fselector/algo_continuous/ReliefF_c.rb
+++ lib/fselector/algo_continuous/ReliefF_c.rb
@@ -9,90 +9,12 @@ module FSelector
 #
 # ref: [Estimating Attributes: Analysis and Extensions of RELIEF](http://www.springerlink.com/content/fp23jh2h0426ww45/)
 #
-  class ReliefF_c < BaseContinuous
-    #
-    # new()
-    #
-    # @param [Integer] m number of samples to be used 
-    #   for estimating feature contribution. max can be
-    #   the number of training samples
-    # @param [Integer] k number of k-nearest neighbor
-    # @param [Hash] data existing data structure
-    #
-    def initialize(m=nil, k=10, data=nil)
-      super(data)
-      @m = m # use all samples
-      @k = (k || 10)  # default 10
-    end
+  class ReliefF_c < BaseReliefF
     
-    private
-    
-    # calculate contribution of each feature (f) across all classes
-    def calc_contribution(f)
-      score = 0.0
-      
-      # use all samples if @m not provided
-      @m = get_sample_size if not @m
-      
-      @m.times do
-        # pick a sample at random
-        rs, rk = pick_a_sample_at_random
-        
-        # find k nearest neighbor for each class
-        nbrs = find_k_nearest_nb(rs, rk)
-        
-        # calc contribution from neighbors
-        score += calc_score(f, rs, rk, nbrs)
-      end
-      
-      s = score / @m
-      
-      set_feature_score(f, :BEST, s)
-    end # calc_contribution
-    
-    
-    # pick a sample at random
-    def pick_a_sample_at_random
-      rk = get_classes[rand(get_classes.size)]
-      rks = get_data[rk]
-      
-      [ rks[rand(rks.size)], rk ]
-    end # pick_a_sample_at_random
-    
-    # # find k nearest neighbors of sample (rs) for each class 
-    def find_k_nearest_nb(rs, rk)
-      nbrs = {}
-      
-      each_class do |k|
-        res = []
-        
-        get_data[k].each do |s|
-          next if s == rs # exclude self
-          
-          d = diff_sample(rs, s, rk, k)
-          res << [d, s]
-        end
-        
-        nbrs[k] = (res.sort { |x, y| x[0] <=> y[0] }[0...@k]).collect { |z| z[1] }
-      end
-      
-      nbrs
-    end # find_k_nearest_nb
-    
-    
-    # difference between two samples
-    def diff_sample(s1, s2, k1, k2)
-      d = 0.0
-            
-      each_feature do |f|
-        d += diff_feature(f, s1, s2, k1, k2)**2
-      end
-      
-      d
-    end # diff_sample
-    
+    private    
     
     # difference beween the feature (f) of two samples
+    # specialized version for continuous feature
     def diff_feature(f, s1, s2, k1, k2)
       d = 0.0
       
@@ -144,7 +66,7 @@ module FSelector
       end
       
       @f2mvp[f][k][mv]
-    end
+    end # calc_p
     
     
     # get normalization unit for each feature
@@ -160,27 +82,6 @@ module FSelector
       
       @f2nu[fi]
     end # get_normalization_unit
-    
-    
-    # calc feature (f) contribution from neighbors
-    def calc_score(f, rs, rk, nbrs)
-      score = 0.0
-      
-      nbrs.each do |k, nbs|
-        if k == rk # near hit
-          nbs.each do |s|
-            score -= (diff_feature(f, rs, s, rk, k)**2/nbs.size)
-          end
-        else # near_miss
-          nbs.each do |s|
-            score += (get_data[k].size/get_sample_size.to_f *
-                     diff_feature(f, rs, s, rk, k)**2/nbs.size)
-          end
-        end
-      end
-      
-      score
-    end
     
     
   end # class
