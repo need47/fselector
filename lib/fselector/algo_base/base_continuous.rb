08c919295619e9eaa5e2bac8fa41ac194d5e7319
Tiejun Cheng <need47@gmail.com>
2012-03-29 19:31:36 -0400

    move base.rb base_continuous.rb base_discrete.rb into algo_base/

--- lib/fselector/algo_base/base_continuous.rb
+++ lib/fselector/algo_base/base_continuous.rb
@@ -1,5 +1,5 @@
-require File.expand_path(File.dirname(__FILE__) + '/algo_continuous/normalizer.rb')
-require File.expand_path(File.dirname(__FILE__) + '/algo_continuous/discretizer.rb')
+require File.expand_path(File.dirname(__FILE__) + '/../algo_continuous/normalizer.rb')
+require File.expand_path(File.dirname(__FILE__) + '/../algo_continuous/discretizer.rb')
 #
 # FSelector: a Ruby gem for feature selection and ranking
 #
