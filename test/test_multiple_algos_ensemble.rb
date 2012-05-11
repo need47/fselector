11015618faa7c948c1edfb42732a973d9f59e07a
Tiejun Cheng <need47@gmail.com>
2012-05-11 15:44:18 -0400

    update example because changes in EnsembleSingle and EnsembleMultiple

--- test/test_multiple_algos_ensemble.rb
+++ test/test_multiple_algos_ensemble.rb
@@ -15,8 +15,7 @@ puts "\n>============> #{File.basename(__FILE__)}"
 
 # test for the weighting type of algorithm
 # use InformationGain
-# no need to use .new
-r = FSelector::IG
+r = FSelector::IG.new
 
 # an ensemble of 40 selectors with 90% data by bootstrap sampling
 re = FSelector::EnsembleSingle.new(r, 40, 0.90, :bootstrap_sampling)
@@ -45,8 +44,7 @@ puts '  # features before (after): ' + re.get_features.size.to_s
 
 # test for the subset selection type of algorithm
 # use INTERACT
-# no need to use .new
-r = FSelector::INTERACT
+r = FSelector::INTERACT.new(0.0001)
 
 # an ensemble of 40 selectors with 90% data by random sampling
 re = FSelector::EnsembleSingle.new(r, 40, 0.90, :random_sampling)
@@ -72,9 +70,8 @@ puts '  # features before (after): ' + re.get_features.size.to_s
 
 # test for the weighting type of algorithms
 # use both InformationGain and Relief_d
-# no need to use .new
-r1 = FSelector::IG
-r2 = FSelector::Relief_d
+r1 = FSelector::IG.new
+r2 = FSelector::Relief_d.new(10)
 
 # an ensemble of two feature selectors (IG and Relief_d)
 re = FSelector::EnsembleMultiple.new(r1, r2)
@@ -103,9 +100,8 @@ puts '  # features before (after): ' + re.get_features.size.to_s
 
 # test for the subset selection type of algorithms
 # use both CFS_d and INTERACT
-# no need to use .new
-r1 = FSelector::CFS_d
-r2 = FSelector::INTERACT
+r1 = FSelector::CFS_d.new
+r2 = FSelector::INTERACT.new
 
 # an ensemble of two feature selectors (CFS_d and INTERACT)
 re = FSelector::EnsembleMultiple.new(r1, r2)
