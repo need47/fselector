b260d7192212fa7e08cbffbbf26098f8e225ead5
Tiejun Cheng <need47@gmail.com>
2012-05-16 16:11:29 -0400

    include Discretizer module

--- lib/fselector/algo_discrete/ReliefF_d.rb
+++ lib/fselector/algo_discrete/ReliefF_d.rb
@@ -9,6 +9,9 @@ module FSelector
 # ref: [Estimating Attributes: Analysis and Extensions of RELIEF](http://www.springerlink.com/content/fp23jh2h0426ww45/)
 #
   class ReliefF_d < BaseReliefF
+    # include Discretizer
+    include Discretizer
+    
     # this algo outputs weight for each feature
     @algo_type = :feature_weighting
        
--- lib/fselector/algo_discrete/Relief_d.rb
+++ lib/fselector/algo_discrete/Relief_d.rb
@@ -10,6 +10,9 @@ module FSelector
 # ref: [The Feature Selection Problem: Traditional Methods and a New Algorithm](http://www.aaai.org/Papers/AAAI/1992/AAAI92-020.pdf)
 #
   class Relief_d < BaseRelief
+    # include Discretizer
+    include Discretizer
+    
     # this algo outputs weight for each feature
     @algo_type = :feature_weighting
     
