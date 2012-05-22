cf45d40272de29c7cd588b501b7a9f383efe02d6
Tiejun Cheng <need47@gmail.com>
2012-05-22 17:23:03 -0400

    update data_from_csv() to read CSV more flexibly add data_from_url() to read on-line dataset specified by a url

--- lib/fselector/fileio.rb
+++ lib/fselector/fileio.rb
@@ -20,6 +20,11 @@
 # @note class labels and features are treated as symbols
 #
 module FileIO
+  # require the open-uri lib for http/https/ftp request
+  require 'open-uri'
+  # require the stringio lib for converting a string into stringio object
+  require 'stringio'
+  
   #
   # read from random data (read only, for test purpose)
   #
@@ -76,20 +81,13 @@ module FileIO
   # -1  3:1 4:1 ...  
   # ....
   #
-  # @param [String] fname file to read from  
+  # @param [Symbol|String|StringIO] fname data source to read from  
   #   :stdin  # read from standard input instead of file
   #
-  def data_from_libsvm(fname=:stdin)
-    data = {}
+  def data_from_libsvm(fname=:stdin)    
+    ifs = get_ifs(fname)
     
-    if fname == :stdin
-      ifs = $stdin
-    elsif not File.exists? fname
-      abort "[#{__FILE__}@#{__LINE__}]: \n"+
-            "  File '#{fname}' does not exist!"
-    else
-      ifs = File.open(fname)
-    end
+    data = {}
     
     ifs.each_line do |ln|
       label, *features = ln.chomp.split(/\s+/)
@@ -116,14 +114,10 @@ module FileIO
   # write to libsvm
   #
   # @param [String] fname file to write  
-  #   :stdout  # write to standard ouput instead of file
+  #   :stdout  # write to standard ouput
   #
   def data_to_libsvm(fname=:stdout)
-    if fname == :stdout
-      ofs = $stdout
-    else
-      ofs = File.open(fname, 'w')
-    end
+    ofs = get_ofs(fname)
     
     # convert class label to integer type
     k2idx = {}
@@ -153,54 +147,82 @@ module FileIO
   #
   # read from csv
   #
-  # file should have the format with the first two rows
-  # specifying features and their data types e.g.  
-  # feat\_name1,feat\_name2,...,feat\_namen  
-  # feat\_type1,feat\_type2,...,feat\_typen  
+  # if no csv_opts supplied, we assume the CSV file in the following format:  
+  # first and second rows contain feature names and types, respectively. 
   # 
-  # and the remaing rows showing data e.g.  
-  # class\_label,feat\_value1,feat\_value2,...,feat\_value3  
-  # ...  
+  #     feat_name1,feat_name2,...,feat_namen  
+  #     feat_type1,feat_type2,...,feat_typen  
+  # 
+  # and the remaing rows containing the data with first column being the class label 
+  #
+  #     class_label,feat_value1,feat_value2,...,feat_value3  
+  #     ...  
   # 
   # allowed feature types (case-insensitive) are:  
   # INTEGER, REAL, NUMERIC, CONTINUOUS, STRING, NOMINAL, CATEGORICAL
   #
-  # @param [String] fname file to read from  
-  #   :stdin  # read from standard input instead of file
+  # @param [Symbol|String|StringIO] fname data source to read from  
+  #   :stdin  # read from standard input
+  # @param [Hash] csv_opts named arguments for csv options     
+  #   :feature\_name\_row => 1,    # row that contains feature names  
+  #   :feature\_type\_row => 2,    # row that contains feature types  
+  #   :feature\_name2type => {},   # a user-supplied hash containing feature name-type pairs 
+  #                                  if no rows specify them. feature must be in the same order 
+  #                                  as it appears in the dataset  
+  #   :class\_label\_column  => 1  # column that contains class labels
   #
-  # @note missing values are allowed, and feature types are stored as lower-case symbols
+  # @note missing values are allowed
   #
-  def data_from_csv(fname=:stdin)
-    data = {}
+  def data_from_csv(fname=:stdin, csv_opts = {})
+    ifs = get_ifs(fname)
     
-    if fname == :stdin
-      ifs = $stdin
-    elsif not File.exists? fname
-      abort "[#{__FILE__}@#{__LINE__}]: \n"+
-            "  File '#{fname}' does not exist!"
-    else
-      ifs = File.open(fname)
+    opts = { # default options, new opts will override old ones
+      :feature_name_row => 1,    # first row contains feature names
+      :feature_type_row => 2,    # second row contains feature types
+      :feature_name2type => {},  # user-supplied feature name-type pairs
+      :class_label_column => 1,  # first column contains class labels
+    }
+    opts.merge!(csv_opts) if csv_opts and csv_opts.class == Hash
+    
+    feature_name_row = opts[:feature_name_row]
+    feature_type_row = opts[:feature_type_row]
+    feature_name2type = opts[:feature_name2type]
+    class_label_column = opts[:class_label_column]
+    
+    # user-supplied feature name-type pairs, this is useful 
+    # when file contains no specific rows for feture names and types
+    if feature_name2type and not feature_name2type.empty?
+      features = feature_name2type.keys.collect { |n| n.to_sym }
+      types = feature_name2type.values.collect { |t| t.downcase.to_sym }
+      # disable name and type rows
+      feature_name_row, feature_type_row = nil, nil
     end
     
-    first_row, second_row = true, true
-    features, types = [], []
+    data = {}
     
     ifs.each_line do |ln|
-      if first_row # first row
-        first_row = false
+      next if ln.blank?
+      
+      if ifs.lineno == feature_name_row # feature names
         features = ln.chomp.split(/,/).to_sym
-      elsif second_row # second row
-        second_row = false
+      elsif ifs.lineno == feature_type_row # second types
         # store feature type as lower-case symbol
         types = ln.chomp.split(/,/).collect { |t| t.downcase.to_sym }
-        if not types.size == features.size
-          abort "[#{__FILE__}@#{__LINE__}]: \n"+
-                "  the first two rows must have same number of fields"
-        end
       else # data rows
-        label, *fvs = ln.chomp.split(/,/)
-        label = label.to_sym
-        data[label] ||= []
+        cells = ln.chomp.split(/,/)
+        
+        if class_label_column <= cells.size
+          label = cells[class_label_column-1]
+          label = label.to_sym
+          data[label] ||= []
+          
+          # remove class label
+          cells.delete_at(class_label_column-1)
+          fvs = cells
+        else
+          abort "[#{__FILE__}@#{__LINE__}]: \n"+
+                "  the class label column can't found!"
+        end              
         
         fs = {}
         fvs.each_with_index do |v, i|
@@ -214,8 +236,7 @@ module FileIO
             #
           else
             abort "[#{__FILE__}@#{__LINE__}]: \n"+
-                  "  please specify correct type "+
-                  "for each feature in the 2nd row"
+                  "  invalid feature type!"
           end
           
           fs[features[i]] = v
@@ -230,10 +251,13 @@ module FileIO
     
     set_data(data)
     set_features(features)
-    # set feature type
+    
+    # feature name-type pairs
+    name2type = {}
     features.each_with_index do |f, i|
-      set_opt(f, types[i])
+      name2type[f] = types[i]
     end
+    set_opt(:feature_type, name2type)
   end # data_from_csv
   
   
@@ -245,18 +269,16 @@ module FileIO
   # and the remaing rows showing data
   #
   # @param [String] fname file to write  
-  #   :stdout  # write to standard ouput instead of file
+  #   :stdout  # write to standard ouput
   #
   def data_to_csv(fname=:stdout)
-    if fname == :stdout
-      ofs = $stdout
-    else
-      ofs = File.open(fname, 'w')
-    end
-     
+    ofs = get_ofs(fname)
+    
+    # feature names 
     ofs.puts get_features.join(',')
+    # feature types
     ofs.puts get_features.collect { |f| 
-      get_opt(f) || :string
+      get_feature_types(f) || :string
     }.join(',')
     
     each_sample do |k, s|
@@ -279,24 +301,17 @@ module FileIO
   #
   # read from WEKA ARFF file
   #
-  # @param [String] fname file to read from  
-  #   :stdin  # read from standard input instead of file
+  # @param [Symbol|String|StringIO] fname data source to read from  
+  #   :stdin  # read from standard input
   # @note it's ok if string containes spaces quoted by quote_char
   #
   def data_from_weka(fname=:stdin, quote_char='"')
-    data = {}
-    
-    if fname == :stdin
-      ifs = $stdin
-    elsif not File.exists? fname
-      abort "[#{__FILE__}@#{__LINE__}]: \n"+
-            "  File '#{fname}' does not exist!"
-    else
-      ifs = File.open(fname)
-    end
+    ifs = get_ifs(fname)
     
     relation, features, classes, types, comments = '', [], [], [], []
     has_class, has_data = false, false
+    
+    data = {}
     
     ifs.each_line do |ln|
       next if ln.blank? # blank lines
@@ -312,7 +327,7 @@ module FileIO
       # class attribute
       elsif ln =~ /^@ATTRIBUTE\s+class\s+{(.+)}/i
         has_class = true
-        classes = $1.split_me(/,\s*/, quote_char).to_sym
+        classes = $1.strip.split_me(/,\s*/, quote_char).to_sym
         classes.each { |k| data[k] = [] }
       # feature attribute (nominal)
       elsif ln =~ /^@ATTRIBUTE\s+(\S+)\s+{(.+)}/i
@@ -377,9 +392,14 @@ module FileIO
     set_classes(classes)
     set_features(features)
     set_opt(:relation, relation)
+    
+    # feature name-type pairs
+    name2type = {}
     features.each_with_index do |f, i|
-      set_opt(f, types[i])
+      name2type[f] =  types[i]
     end
+    set_opt(:feature_type, name2type)
+    
     set_opt(:comments, comments) if not comments.empty?
   end # data_from_weak
   
@@ -388,16 +408,12 @@ module FileIO
   # write to WEKA ARFF file
   #
   # @param [String] fname file to write  
-  #   :stdout  # write to standard ouput instead of file
+  #   :stdout  # write to standard ouput
   # @param [Symbol] format sparse or regular ARFF  
   #   :sparse  # sparse ARFF, otherwise regular ARFF
   #
   def data_to_weka(fname=:stdout, format=nil)
-    if fname == :stdout
-      ofs = $stdout
-    else
-      ofs = File.open(fname, 'w')
-    end
+    ofs = get_ofs(fname)
     
     # comments
     comments = get_opt(:comments)
@@ -419,7 +435,7 @@ module FileIO
     # feature attribute
     each_feature do |f|
       ofs.print "@ATTRIBUTE #{f} "
-      type = get_opt(f) # feature type
+      type = get_feature_types(f)
       if type
         if type == :nominal
           ofs.puts "{#{get_feature_values(f).uniq.sort.join(',')}}"
@@ -464,9 +480,72 @@ module FileIO
     
     # close file
     ofs.close if not ofs == $stdout
-  end
+  end # data_to_weka
+  
+  
+  # read data from url
+  #
+  # @param [String] url url of on-line dataset
+  # @param [Symbol] format allowed formats are:  
+  #   :libsvm  # LibSVM file  
+  #   :csv     # csv file  
+  #   :weka    # Weka ARFF file
+  # @param [Any] args arguments associated with format
+  #
+  def data_from_url(url, format, *args)
+    format = format.downcase.to_sym
+    
+    if not [:libsvm, :csv, :weka].include? format
+      abort "[#{__FILE__}@#{__LINE__}]: \n"+
+            "  only CSV, LibSVM and Weka file formats are supported!"
+    end
+    
+    uri = URI.parse(URI.encode(url))
+    
+    data_src = StringIO.new(uri.read)
+    
+    if format == :csv
+      data_from_csv(data_src, *args)
+    elsif format == :libsvm
+      data_from_libsvm(data_src)
+    else # weka
+      data_from_weka(data_src, *args)
+    end
+  end # data_from_url
   
   private
+  
+  # get the input file handler
+  def get_ifs(fname)
+    # read from standard input by default
+    if fname == :stdin
+      ifs = $stdin
+    # read from string if it is a StringIO
+    elsif fname.class == StringIO
+      ifs = fname
+    # read from file if file exists
+    elsif File.exists? fname
+      ifs = File.open(fname)
+    else
+      abort "[#{__FILE__}@#{__LINE__}]: \n"+
+            "  invalid data source!"
+    end
+    
+    ifs
+  end
+  
+  
+  # get the ouput file handler
+  def get_ofs(fname)
+    if fname == :stdout
+      ofs = $stdout
+    else
+      ofs = File.open(fname, 'w')
+    end
+    
+    ofs
+  end
+  
   
   # handle and add each feature for WEKA format
   #
