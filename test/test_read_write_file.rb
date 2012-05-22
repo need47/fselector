459cc81d0aadb6cf1bffb222782972e344880714
Tiejun Cheng <need47@gmail.com>
2012-05-22 17:07:36 -0400

    add test example for data_from_url()

--- test/test_read_write_file.rb
+++ test/test_read_write_file.rb
@@ -6,7 +6,7 @@ require 'fselector'
 puts "\n>============> #{File.basename(__FILE__)}"
 
 # use InformationGain as a feature ranking algorithm
-r = FSelector::InformationGain.new
+r = FSelector::Relief_c.new
 
 # example 1
 #
@@ -36,5 +36,28 @@ r.data_from_weka('test/iris.arff')
 # write data to LibSVM format
 puts "  write to LibSVM file"
 r.data_to_libsvm('test/iris.libsvm')
+
+
+# example 3
+#
+
+# read Iris data online
+puts "  read from on-line Iris dataset"
+csv_url = "http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
+# supply feature name-type pairs as the dataset from the above url does not specify them
+# feature must be in the same order as it appears in the dataset
+name2type = {
+    :'sepal-length' => :numeric,
+    :'sepal-width' => :numeric,
+    :'petal-length' => :numeric,
+    :'petal-width' => :numeric
+}
+
+# read data (CSV file format)
+r.data_from_url(csv_url, :csv, :class_label_column=>5, :feature_name2type=>name2type)
+
+# => 150
+puts "  numboer of samples read: %d" % r.get_sample_size
 
 puts "<============< #{File.basename(__FILE__)}"
